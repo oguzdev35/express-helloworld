307bb0c65c51eeb6bd3a31bf272f6143fbd692b6
oguzdev35 <oguzdev35@gmail.com>
2021-10-10 06:03:14 +0300

    ds

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get("/", (req, res) => {
   res.status(200).send("Hello World");
 });
 
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
   console.log("> Server Listen At 3000");
+});
+process.on("SIGTERM", function () {
+  server.close(function () {
+    console.log("Finished all requests");
+  });
 });
